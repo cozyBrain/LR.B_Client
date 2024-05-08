5a2f9c4eaaf76c9ef8f4abd430b7c3db9f76a2e6
CSharpNeNe <CSharpNeNe@users.noreply.github.com>
2024-05-08 15:32:17 +0900

    Code refactor.
    OfflineMenu.gd
    start_menu.gd
    terminal.gd

--- start_menu/start_menu.gd
+++ start_menu/start_menu.gd
@@ -4,67 +4,73 @@ extends Control
 var after_menu  # can be %MainMenu or %OfflineMenu
 var previous_menu
 
-
 func _ready():
-#	TranslationServer.set_locale("kr")
-#	%LoginButton.text = tr("login")
-	
-	# setup window
+	setup()
+	previous_menu = %MainMenu
+	fade_in_transition()
+
+func setup():
+	setup_window()
+	connect_signals()
+	setup_menu_visibility()
+
+func setup_window():
 	window.scale_with_screen_size(window.size_main_menu)
 	window.center()
 
-	# connect start_menu signals to the terminal
+func connect_signals():
 	%ID_Input.grab_focus()
 	(%LoginButton as Button).pressed.connect(terminal._on_login_button_pressed)
 	(%RegisterButton as Button).pressed.connect(terminal._on_register_button_pressed)
-	
-	# set visible for MainMenu only for start_menu
+
+func setup_menu_visibility():
 	%MainMenu.visible = true
 	%OfflineMenu.visible = false
-	
-	previous_menu = %MainMenu
-	
-	# GUI transition fade_in
+
+func fade_in_transition():
 	%Transition/ColorRect.visible = true
 	%Transition.play("fade_in")
 
-
 func _input(event):
-	# login when user press enter
 	if event is InputEventKey and event.pressed:
 		if Input.is_action_pressed("ui_accept"):
 			(%LoginButton).grab_focus()
 
 func _on_offline_button_pressed():
-	after_menu = %OfflineMenu
-	%Transition.play("fade_out")
-	
-	
+	switch_menu(%OfflineMenu)
+
 func _on_main_menu_button_pressed():
-	after_menu = %MainMenu
+	switch_menu(%MainMenu)
+
+func switch_menu(menu):
+	after_menu = menu
 	%Transition.play("fade_out")
 
-
-# switch menu
 func _on_transition_animation_finished(anim_name):
 	if anim_name == "fade_out":
-		previous_menu.visible = false
-		if after_menu.has_method("appear"):
-			after_menu.call("appear")
-		else:
-			after_menu.visible=true
-		
-		set_process_input(false)
-		match after_menu.name:
-			"OfflineMenu":
-				window.scale_with_screen_size(window.size_offline_menu)
-				window.center()
-			"MainMenu":
-				set_process_input(true)
-				window.scale_with_screen_size(window.size_main_menu)
-				window.center()
-		
-		# change scene before fade_in
-		%Transition.play("fade_in")
-		# update previous menu for next transition
-		previous_menu = after_menu
+		hide_previous_menu()
+		show_next_menu()
+		update_window()
+		fade_in_transition()
+		update_previous_menu()
+
+func hide_previous_menu():
+	previous_menu.visible = false
+
+func show_next_menu():
+	if after_menu.has_method("appear"):
+		after_menu.call("appear")
+	else:
+		after_menu.visible = true
+
+func update_window():
+	match after_menu.name:
+		"OfflineMenu":
+			window.scale_with_screen_size(window.size_offline_menu)
+		"MainMenu":
+			window.scale_with_screen_size(window.size_main_menu)
+			set_process_input(true)
+	window.center()
+
+func update_previous_menu():
+	previous_menu = after_menu
--- terminal.gd
+++ terminal.gd
@@ -2,7 +2,6 @@ extends Node
 
 @onready var local_hub = get_node("/root/hub")
 
-
 var virtual_remote_hub # ref for this script
 const virtual_remote_hub_dflt_name = "virtual_remote_hub"
 var virtual_remote_hub_offline = preload("res://session/virtual_remote_hub/virtual_remote_hub_offline.tscn")
@@ -16,7 +15,8 @@ var connection_buffer : PackedByteArray # buffer to be sent when server connecte
 
 func set_up_connection() -> int:
 	var err := enet.create_client("127.0.0.1", 7000)
-	if err: return err
+	if err:
+		return err
 	err = enet.get_host().dtls_client_setup(
 		"dtls_test.com", # common_name of cert. if different, handshake fail!
 		TLSOptions.client(
@@ -25,14 +25,12 @@ func set_up_connection() -> int:
 	)
 	return err
 
-
 func _ready():
 	# connect signals
 	multiplayer.connected_to_server.connect(_on_connected_to_server)
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
 	multiplayer.connection_failed.connect(_on_connection_failed)
 	multiplayer.peer_packet.connect(_on_peer_packet)
-
 
 func _on_connected_to_server():
 	print("connected to the server.")
@@ -42,7 +40,6 @@ func _on_server_disconnected():
 	multiplayer.multiplayer_peer.close()
 	print("server disconnected")
 
-
 func _on_connection_failed():
 	OS.alert("connection failed")
 	# reset connection_buffer
@@ -51,13 +48,12 @@ func _on_connection_failed():
 func _on_peer_packet(id: int, packet: PackedByteArray):
 	print(id, ": ", packet.get_string_from_ascii())
 
-
-
-#region _on_login_and_register_button_pressed
 func _on_login_button_pressed() -> void:
 	# set up connection
 	var err := set_up_connection()
-	if err: printerr(error_string(err))
+	if err:
+		printerr(error_string(err))
+		return
 	
 	# get ID, PW to login
 	var ID : String = (local_hub.get_node("start_menu").get_node("%ID_Input") as LineEdit).get_text()
@@ -78,7 +74,9 @@ func _on_login_button_pressed() -> void:
 func _on_register_button_pressed() -> void:
 	# set up connection
 	var err := set_up_connection()
-	if err: printerr(error_string(err))
+	if err:
+		printerr(error_string(err))
+		return
 	
 	# get ID, PW to register
 	var ID : String = (local_hub.get_node("start_menu").get_node("%ID_Input") as LineEdit).get_text()
@@ -95,8 +93,6 @@ func _on_register_button_pressed() -> void:
 	connection_buffer = json_string.to_utf8_buffer()
 	# connect to server
 	multiplayer.set_multiplayer_peer(enet)
-#endregion
-
 
 func enter_session(session_config : Dictionary) -> void:
 	print(session_config)
@@ -122,8 +118,6 @@ func enter_session(session_config : Dictionary) -> void:
 	# create space_projection and player
 	local_hub.add_child(space_projection.instantiate())
 
-
-
 func exit_session() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
@@ -131,35 +125,34 @@ func exit_session() -> void:
 	# create start_menu again
 	local_hub.add_child(start_menu.instantiate())
 
-
 func send_packet(packet: PackedByteArray) -> void:
 	(multiplayer as SceneMultiplayer).send_bytes(packet)
 
-func arr_send_packet(arr: Array) -> void: ## useful function
+func arr_send_packet(arr: Array) -> void:
+	# useful function
 	pass
 
-func handle(data: Dictionary):
+func handle(data: Dictionary) -> void:
 	var selected_hub = data["Hub"]
 	selected_hub.handle(data)
 
-
-
-# set virtual_remote_hub offline or online
-func switch_virtual_remote_hub_to_offline():
+func switch_virtual_remote_hub_to_offline() -> void:
 	if not virtual_remote_hub == null:
 		local_hub.remove_child(virtual_remote_hub)
 	var i := virtual_remote_hub_offline.instantiate()
 	i.name = virtual_remote_hub_dflt_name
 	local_hub.add_child(i)
 	virtual_remote_hub = i
-func switch_virtual_remote_hub_to_online():
+
+func switch_virtual_remote_hub_to_online() -> void:
 	if not virtual_remote_hub == null:
 		local_hub.remove_child(virtual_remote_hub)
 	var i := virtual_remote_hub_online.instantiate()
 	i.name = virtual_remote_hub_dflt_name
 	local_hub.add_child(i)
 	virtual_remote_hub = i
-func virtual_remote_hub_to_offline():
+
+func virtual_remote_hub_to_offline() -> void:
 	var i := virtual_remote_hub_offline.instantiate()
 	i.name = virtual_remote_hub_dflt_name
 	local_hub.add_child(i)
