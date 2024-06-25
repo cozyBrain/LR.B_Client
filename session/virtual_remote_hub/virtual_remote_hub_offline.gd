a677572872e81e11cadab01feee1d6a69681d52e
cozyBrain <cozy.brain2020@gmail.com>
2024-06-25 22:07:14 +0900

    Initial implementation of running handle function in a separate thread using new h function
    New h function to execute the handle function in a separate thread in virtual_remote_hub_offline.gd. This change ensures that the module handling process operates independently, preventing blocking of the main thread and enhancing overall performance and responsiveness.

--- session/virtual_remote_hub/virtual_remote_hub_offline.gd
+++ session/virtual_remote_hub/virtual_remote_hub_offline.gd
@@ -6,6 +6,9 @@ extends Node
 @onready var player_module_container = %offline_player/player_modules
 @onready var space_module_container = $space/space_modules
 
+var handler := Thread.new()
+
+
 func handle(data : Dictionary):
 	# select module container
 	var module_container_selection = data["ModuleContainer"] as String
@@ -26,5 +29,37 @@ func handle(data : Dictionary):
 		printerr("Invalid Module selection: ", module_selection)
 		return
 	# use module
-	module.handle(data["Content"])
-	
+	#module.handle(data["Content"])
+	if handler.is_started():
+		handler.wait_to_finish()
+	handler.start(h.bind(module, data["Content"]))
+
+func h(module: Node, content):
+	module.call_thread_safe("handle", content)
+
+	## Create task to observe target_chunk.
+	#var task := PlayerUnifiedChunkObserver.observing_task.new()
+	## Register the task to clear the task later.
+	#requested_tasks[task] = true
+	#task.add_chunk_to_observe([chunk_pos_to_observe])
+	#unified_chunk_observer.observe(task)
+	## Wait until the chunk is guaranteed to be accessible.
+	#await task.done
+	## Create node.
+	#terminal.handle(
+		#{
+			#"Hub": 				terminal.virtual_remote_hub,
+			#"ModuleContainer": 	"Player",
+			#"Module": 			tool_name,
+			#"Content": {
+				#"Request": 		"create",
+				#"TaskID":		task.get_instance_id(),
+				#"id": 			node_selection.hash(),
+				#"pos":			[pos.x, pos.y, pos.z]
+			#},
+		#}
+	#)
+
+func _exit_tree():
+	if handler.is_started():
+		handler.wait_to_finish()
